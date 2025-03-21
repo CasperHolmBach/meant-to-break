c4cc483db7ee6c9d923852c43caab13b29d83f24
Rasmus <liltorp03@gmail.com>
2025-03-21 15:45:26 +0100

    added fpscontroller.cs & audio

---
+++ meant-to-break/Assets/Scripts/FPSController.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class FPSController : MonoBehaviour
+{
+    public float moveSpeed = 6f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 1.5f;
+    public float mouseSensitivity = 100f;
+
+    public Transform cameraTransform;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+    private float xRotation = 0f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        // Lås cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        MovePlayer();
+        RotateCamera();
+    }
+
+    void MovePlayer()
+    {
+        // Ground check
+        isGrounded = controller.isGrounded;
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        // Input
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        // Jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+        // Gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void RotateCamera()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
