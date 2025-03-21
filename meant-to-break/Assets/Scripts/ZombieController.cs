0204f3f88c0bf873b4d30b4297e30216426b7fbc
Superslammer <tobiasgaifort@outlook.dk>
2025-03-21 16:24:51 +0100

    Initial work on zombie controller

---
+++ meant-to-break/Assets/Scripts/ZombieController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieController : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Vector3 destination;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(destination);
+    }
+}
